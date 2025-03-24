d0d9c7ba8e51a7aa052715078f44bdc405f4ac80
miladjavadi <miladjavd@gmail.com>
2025-03-24 18:32:38 +0100

    implement GAN model training script

---
+++ gantransfer/ganmodel.py
@@ -0,0 +1,91 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import numpy as np
+
+import dac
+import torchaudio
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv1d(1024, 256, kernel_size=5, padding="same", padding_mode="reflect"),
+            nn.LeakyReLU(),
+            nn.Conv1d(256, 64, kernel_size=5, padding="same", padding_mode="reflect"),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.Conv1d(64, 256, kernel_size=5, padding="same", padding_mode="reflect"),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Conv1d(256, 1024, kernel_size=5, padding="same", padding_mode="reflect")
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+class Discriminator(nn.Module):
+    # generate discriminator score between 0 and 1.
+    def __init__(self, block_length_in_samples, block_length_in_frames):
+        super(Discriminator, self).__init__()
+
+        self.block_length_in_samples = block_length_in_samples
+        self.block_length_in_frames = block_length_in_frames
+
+        self.embedding_path = nn.Sequential(
+            nn.Conv1d(1024, 256, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv1d(256, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+        )
+
+        self.waveform_path = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(),
+            nn.Conv1d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Conv1d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU()
+        )
+
+        self.combined_layer = nn.Sequential(
+            nn.Linear((64*(block_length_in_frames//4) + 64*(block_length_in_samples//8)), 512),
+            nn.LeakyReLU(),
+            nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, embedding, waveform):
+        embedding = embedding.to(torch.float32)
+        waveform = waveform.to(torch.float32)
+
+        embedding_features = self.embedding_path(embedding)
+        waveform_features = self.waveform_path(waveform)
+
+        combined_features = torch.cat([embedding_features.flatten(1, -1), waveform_features.flatten(1, -1)], dim=1)
+        output_probability = self.combined_layer(combined_features)
+
+
+        return output_probability
+
+class PairedWaveformDataset(torch.utils.data.Dataset):
+    def __init__(self, target_data, corpus_data):
+        if target_data.shape != corpus_data.shape:
+            raise Exception(f"Target dataset and corpus dataset must have the same size (target dataset has shape {target_data.shape}, while corpus dataset has shape {corpus_data.shape})")
+        self.target_data = target_data
+        self.corpus_data = corpus_data
+    
+    def __len__(self):
+        return self.target_data.shape[0]
+    
+    def __getitem__(self, idx):
+        x = self.target_data[idx]
+        y = self.corpus_data[idx]
+        return x, y
---
+++ gantransfer/train.py
@@ -0,0 +1,157 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import numpy as np
+import datetime
+
+import dac
+import torchaudio
+from ganmodel import Generator, Discriminator, PairedWaveformDataset
+
+# Load audio file
+def load_mono(file_name: str, target_sr: int) -> torch.FloatTensor:
+    audio, sr = torchaudio.load(file_name)
+    if audio.shape[0] > 1:
+        audio = audio.mean(0, keepdim=True)
+
+    if sr != target_sr:
+        audio = torchaudio.functional.resample(audio, sr, target_sr)
+        sr = target_sr
+    audio = audio.clamp(-1, 1)
+    return audio
+
+def reshape_dataset(waveforms: list[torch.FloatTensor], block_length_in_samples: int) -> torch.FloatTensor:
+    # List([1 x waveform_length]) -> [n_blocks x 1 x block_lengths_in_samples]
+
+    dataset = torch.zeros((0, block_length_in_samples), device=waveforms[0].device)
+    for waveform in waveforms:
+        # trim waveform to whole number of block lengths
+        waveform = waveform[:,:((waveform.shape[1]//block_length_in_samples)*block_length_in_samples)]
+
+        # reshape waveform into blocks
+        blocks = torch.reshape(waveform, (-1, block_length_in_samples))
+        dataset = torch.cat((dataset, blocks), dim=0)
+        dataset = dataset.unsqueeze(1)
+    
+    return dataset
+
+def prepare_dataloader(target_dir: str, corpus_dir: str, block_length_in_samples: int, batch_size: int, model_sr: int, device: str):
+    target_files = os.listdir(target_dir)
+    corpus_files = os.listdir(corpus_dir)
+
+    # load in all waveforms
+    target_waveforms = [load_mono((f"{target_dir}/{file}").to(device), model_sr) for file in target_files if file[-4:] == ".wav"]
+    corpus_waveforms = [load_mono((f"{corpus_dir}/{file}").to(device), model_sr) for file in corpus_files if file[-4:] == ".wav"]
+
+    target_dataset = reshape_dataset(target_waveforms, block_length_in_samples)
+    corpus_dataset = reshape_dataset(corpus_waveforms, block_length_in_samples)
+    paired_dataset = PairedWaveformDataset(target_dataset, corpus_dataset)
+    dataloader = torch.utils.data.DataLoader(paired_dataset, batch_size=batch_size, shuffle=True)
+
+    return dataloader
+
+def training_procedure(gen_model, discr_model, dac_model, dataloader, epochs):
+    embedding_loss_fn = nn.MSELoss()
+    adversarial_loss_fn = nn.BCELoss()
+    lambda_embedding = 100
+
+    gen_optimizer = optim.Adam(gen_model.parameters(), lr=0.00002, betas=(0.5, 0.999))
+    discr_optimizer = optim.Adam(discr_model.parameters(), lr=0.00002, betas=(0.5, 0.999))
+
+    real_label = 1
+    fake_label = 0
+
+    for i in range(epochs):
+        for batch_nr, (target, ref) in enumerate(dataloader):
+            print(f"Epoch: {i+1}/{epochs}, Batch: {batch_nr+1}/{len(dataloader)}")
+
+            with torch.no_grad():
+                Z_target = dac_model.encode(target)[0]
+                Z_ref = dac_model.encode(ref)[0]
+
+            # train discriminator
+            Z_transformed = gen_model(Z_target).detach()
+            
+            with torch.no_grad():
+                transformed_decoded = dac_model.decode(Z_transformed)
+
+            ref = ref[:,:,:transformed_decoded.shape[2]] # trim tail of reference that is lost when decoding
+            
+            d_real = discr_model(Z_target, ref)
+            d_fake = discr_model(Z_target, transformed_decoded)
+
+            real_labels = torch.full(d_real.shape, real_label, device=discr_model.device, dtype=torch.float32)
+            real_adversarial_loss = adversarial_loss_fn(d_real, real_labels)
+
+            fake_labels = torch.full(d_fake.shape, fake_label, device=discr_model.device, dtype=torch.float32)
+            fake_adversarial_loss = adversarial_loss_fn(d_fake, fake_labels)
+
+            discr_loss = real_adversarial_loss + fake_adversarial_loss
+            discr_optimizer.zero_grad()
+            discr_loss.backward(retain_graph=True)
+            discr_optimizer.step()
+
+            # train generator
+            
+            Z_transformed = gen_model(Z_target)
+            embedding_loss = embedding_loss_fn(Z_transformed, Z_ref)
+
+            d_fake = discr_model(Z_target, transformed_decoded)
+            fake_adversarial_loss = adversarial_loss_fn(d_fake, fake_labels)
+
+            gen_loss = 1/fake_adversarial_loss + lambda_embedding * embedding_loss
+            gen_optimizer.zero_grad()
+            gen_loss.backward()
+            gen_optimizer.step()
+
+def main(args):
+    target_dir = args.targetdir
+    corpus_dir = args.corpusdir
+    tempo = args.tempo
+    subdivs = args.subdiv
+    batch_size = args.batchsize
+    max_epochs = args.maxepochs
+
+    timecode = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    dac_model = dac.DAC.load(dac.utils.download()).to(device)
+    model_sr = dac_model.sample_rate
+    block_length_in_samples = int(model_sr*60/(tempo*subdivs/4))
+
+    dataloader = prepare_dataloader(target_dir, corpus_dir, block_length_in_samples, batch_size, model_sr, device)
+
+    # the length of an audio block may be altered during decoding.
+    # thus, a second block sample length must be passed to the discriminator
+    with torch.inference_mode():
+        dummy_frame = dac_model.encode(dataloader[0][0].unsqueeze(0))[0]
+        block_length_in_frames = dummy_frame.shape[2]
+        output_block_length_in_samples = dac_model.decode(dummy_frame).shape[2]
+
+    gen_model = Generator().to(device)
+    discr_model = Discriminator().to(device)
+
+    training_procedure(gen_model, discr_model, dac_model, dataloader, max_epochs)
+
+    torch.save({"gen_model": gen_model.state_dict(), "discr_model": discr_model.state_dict(), "block_length_in_samples": output_block_length_in_samples, "block_length_in_frames": block_length_in_frames}, f"model_{timecode}.pth")
+
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description="Train GAN-based timbre transfer model using paired target/corpus datasets.\n"
+    "File pairs must have the same names within their respective directories.\n"
+    "For instance: <target_dir>/x.wav should have a corresponding <corpus_dir>/x.wav.")
+
+    parser.add_argument("--targetdir", help="Location of target audio files.", type=str, metavar="path_to_directory", nargs=1, required=True)
+    parser.add_argument("--corpusdir", help="Location of corpus audio files.", type=str, metavar="path_to_directory", nargs=1, required=True)
+    parser.add_argument("--tempo", help="Reference tempo against which to divide audio blocks. Should ideally match the tempo of the audio data.", type=float, metavar="bpm", nargs=1, default=90)
+    parser.add_argument("--subdiv", help="Subdivisions against which to divide audio blocks. For instance, \"--tempo 90 --subdiv 8\" means that audio waveforms will be divided into 1/8th note long chunks at 90 BPM.", type=int, metavar="subdivisions", nargs=1, default=8)
+    parser.add_argument("--batchsize", help="Number of data point pairs per mini-batch.", type=int, metavar="batch_size", nargs=1, default=16)
+    parser.add_argument("--maxepochs", help="Maximum number of training epochs.", type=int, metavar="epochs", nargs=1, default=1000)
+    args=parser.parse_args()
+    main(args)
+
