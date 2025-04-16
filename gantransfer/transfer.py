6d90f357a56cc04382f7e7fab6bd5a271838a83b
miladjavadi <miladjavd@gmail.com>
2025-04-16 18:31:18 +0200

    create script for transforming new audio data

---
+++ gantransfer/transfer.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import numpy as np
+import datetime
+
+import dac
+import torchaudio
+from ganmodel import Generator, Discriminator, PairedWaveformDataset
+from train import load_mono
+
+def main(args):
+    input_path = args.input
+    chkpt_path = args.chkpt
+
+    gen_model, discr_model, block_length_in_samples, block_length_in_frames = torch.load(chkpt_path)
+    gen_model.eval()
+    discr_model.eval()
+    dac_model = dac.DAC.load(dac.utils.download()).to(device)
+    model_sr = dac_model.sample_rate
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    input_waveform = load_mono(input_path, model_sr).to(device)
+    # trim waveform to whole number of block lengths
+    input_waveform = input_waveform[:,:((input_waveform.shape[1]//block_length_in_samples)*block_length_in_samples)]
+
+    # reshape waveform into blocks
+    input_blocks = torch.reshape(input_waveform, (-1, block_length_in_samples))
+    input_blocks = input_blocks.unsqueeze(1)
+
+    with torch.inference_mode():
+        input_embeddings = dac_model.encode(input_blocks)[0]
+
+        transformed_embeddings = gen_model(input_embeddings)
+
+        transformed_embeddings = dac_model.quantizer(transformed_embeddings, None)[0]
+
+        reconstruction = dac_model.decode(transformed_embeddings)
+
+    reconstructed_waveform = reconstruction.flatten()
+
+    torchaudio.save(f"{input_path}_transformed.wav", reconstructed_waveform, model_sr)
+
+
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser(description="Transform input audio data using pre-trained generator model.\n")
+    # "File pairs must have the same names within their respective directories.\n"
+    # "For instance: <query_dir>/x.wav should have a corresponding <target_dir>/x.wav.")
+
+    parser.add_argument("--input", help="Path to input audio file.", type=str, metavar="path", required=True)
+    parser.add_argument("--chkpt", help="Path to model checkpoint.", type=str, metavar="path", required=True)
+    args=parser.parse_args()
+    main(args)
