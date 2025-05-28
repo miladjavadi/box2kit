4f715c84315641b1a01c8def60cd38b7ecb5cba8
miladjavadi <miladjavd@gmail.com>
2025-05-28 15:57:00 +0200

    another other typo fix

--- autoconcat/model.py
+++ autoconcat/model.py
@@ -101,7 +101,7 @@ class AutoConcatenator():
             for block in input_embeddings:
                 transformed_embeddings = torch.cat((transformed_embeddings, self.salt(block, query_latents, target_latents, codec, max_steps, tolerance).unsqueeze(0)), dim=0)
 
-            batched_transformed_embeddings = batch_partition(batched_transformed_embeddings, batch_size)
+            batched_transformed_embeddings = batch_partition(transformed_embeddings, batch_size)
             batched_reconstruction = [codec.decode(batch) for batch in batched_transformed_embeddings]
 
 
