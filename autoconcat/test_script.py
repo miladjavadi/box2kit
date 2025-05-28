73f44bb04ba3b8d1001f84e003410ea991fc83c2
miladjavadi <miladjavd@gmail.com>
2025-05-28 18:22:34 +0200

    testing

--- autoconcat/test_script.py
+++ autoconcat/test_script.py
@@ -1,16 +1,29 @@
-import numpy as np
+import torch
+import torchaudio
+import os
 
-from model import PairedCorpus, AutoConcatenator
+from autoconcat.model import PairedCorpus, AutoConcatenator
+from gantransfer.train import load_mono
 
-queries = np.arange(-10, 10).reshape(-1, 1, 1) * np.ones((20, 2, 4))
+queries = torch.arange(-10, 10, device="cpu").reshape(-1, 1, 1) * torch.ones((20, 2, 4), device="cpu")
 
-targets = np.arange(40, 60).reshape(-1, 1, 1) * np.ones((20, 2, 4))
+targets = torch.arange(40, 60, device="cpu").reshape(-1, 1, 1) * torch.ones((20, 2, 4), device="cpu")
 
-corpus = PairedCorpus(queries, targets)
+corpus = PairedCorpus(queries, targets, 2)
 model = AutoConcatenator(corpus)
 
-input = -9.1 * np.ones((2, 4))
+input = 0.4 * torch.ones((2, 4), device="cpu")
 
-output = model.quantize_transfer(input)
+output = model.quantize_transfer(input, queries, targets)
 
 print(output)
+
+files = sorted(os.listdir("training_data/beatbox"))
+
+super_vector = torch.zeros((1,0), device="cpu")
+for file in files:
+    if file[-4:] == ".wav":
+        audio = load_mono(file, 48000).to("cpu")
+        super_vector = torch.cat((super_vector, audio), dim=1)
+
+print(super_vector.shape[1]/48000)
