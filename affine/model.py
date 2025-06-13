acb1b8f354e98a7f44323f58ce03b7a696465a10
miladjavadi <miladjavd@gmail.com>
2025-06-13 17:57:48 +0200

    passionfruit

--- affine/model.py
+++ affine/model.py
@@ -189,7 +189,7 @@ class GenerationCallback(Callback):
             test_segs = reshape_data(test_wave, pl_module.input_block_length).to(pl_module.device)
             with torch.inference_mode():
                 test_frames = pl_module.codec.encode(test_segs)[0]
-                reconstructed_wave = torch.cat([pl_module(seg.unsqueeze(0))[0] for seg in test_frames], dim=2).squeeze(0)
+                reconstructed_wave = torch.cat([pl_module(seg.unsqueeze(0)) for seg in test_frames], dim=2).squeeze(0)
 
             torchaudio.save(f"{self.out_dir}/epoch_{epoch}.wav", reconstructed_wave.cpu(), pl_module.sr)
         return super().on_train_epoch_end(trainer, pl_module)
@@ -200,7 +200,7 @@ class GenerationCallback(Callback):
             test_segs = reshape_data(test_wave, pl_module.input_block_length).to(pl_module.device)
             with torch.inference_mode():
                 test_frames = pl_module.codec.encode(test_segs)[0]
-                reconstructed_wave = torch.cat([pl_module(seg.unsqueeze(0))[0] for seg in test_frames], dim=2).squeeze(0)
+                reconstructed_wave = torch.cat([pl_module(seg.unsqueeze(0)) for seg in test_frames], dim=2).squeeze(0)
 
             torchaudio.save(f"{self.out_dir}/epoch_{pl_module.trainer.current_epoch}.wav", reconstructed_wave.cpu(), pl_module.sr)
         return super().on_train_end(trainer, pl_module)
