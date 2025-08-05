807f8edeb87009c1a02594fc652f9b190db029fc
miladjavadi <miladjavd@gmail.com>
2025-08-05 18:39:53 +0200

    back in bidneh

--- autoconcat/train_test_new.py
+++ autoconcat/train_test_new.py
@@ -2,6 +2,7 @@ import dac
 import torchaudio
 from autoconcat.model import PairedCodebook, MatchSearchTransfer
 import torch
+from autoconcat.util import batch_partition
 
 from utils.load_data import load_dir, reshape_data
 
@@ -10,6 +11,11 @@ def train_val_split(data, split=0.8):
     perm = torch.randperm(n)
     split_n = int(n * split)
     return data[perm[:split_n]], data[perm[split_n:]]
+
+def safe_encode(data, codec, batch_size=64):
+    latents = [codec.encode(waveform)[0] for waveform in batch_partition(data, batch_size)] # save latent code indices to save space
+    latents = torch.cat(latents, dim=0)
+    return latents
 
 def main():
 
@@ -27,11 +33,19 @@ def main():
     target_waveform_segs = reshape_data(target_waves, seg_length_in_samples)
     output_waveform_segs = reshape_data(target_waves, seg_length_in_samples)
 
-    paired_segs = torch.stack((target_waveform_segs, output_waveform_segs), dim=1)
+    paired_waveform_segs = torch.cat((target_waveform_segs, output_waveform_segs), dim=1) # cat along channel direction
 
-    train_data, val_data = train_val_split(paired_segs, 0.8)
+    train_waveform_data, val_waveform_data = train_val_split(paired_waveform_segs, 0.8)
 
-    print(train_data.shape)
+    train_data = safe_encode(train_waveform_data)
+    val_data = safe_encode(val_waveform_data)
+
+    codebook = PairedCodebook(train_data, val_data, 512)
+
+    print(codebook.codebook.shape)
+    gen_model = MatchSearchTransfer(codebook)
+
+    
 
 if __name__ == "__main__":
     main()
