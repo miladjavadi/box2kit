04827e982c807e2ff2dc86e4fca5b71fc36812da
miladjavadi <miladjavd@gmail.com>
2025-08-14 19:00:29 +0200

    goomba

--- box2kit/svae/model.py
+++ box2kit/svae/model.py
@@ -300,6 +300,7 @@ class TransferGAN(LightningVAE):
         reconstruction_loss = fullband_reconstruction_loss + multiband_reconstruction_loss
 
         kl_div = torch.mean(self.model.prior(mu, sigma), 0)
+        print(kl_div.shape)
 
         if self.adversarial_phase:
             stft_gen = torch.stft(y_hat.squeeze(1), self.discriminator.nfft, window=torch.hann_window(self.discriminator.nfft, device=y_hat.device), return_complex=True).abs()
@@ -449,7 +450,6 @@ class MOGPrior(nn.Module):
     def kld_estimate(self, post_mean, post_var):
         weights = torch.softmax(self.weight_logits, 0)
 
-        print(post_mean.shape)
         kld_components = torch.stack([kld_component(mean.reshape(1, -1, 1), var.reshape(1, -1, 1), post_mean, post_var) for (mean, var) in zip(self.means, self.variances)], 1)
         exp_sum = torch.sum(weights.reshape(1, -1, 1, 1) * torch.exp(-kld_components), 1)
         elbo = -torch.log(exp_sum)
