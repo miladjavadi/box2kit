17349ca0c20472a66971322ab21bd91b49342304
miladjavadi <miladjavd@gmail.com>
2025-09-23 16:08:53 +0200

    anti-drums

--- box2kit/affine/silence_test.py
+++ box2kit/affine/silence_test.py
@@ -1,12 +1,16 @@
 import dac
 import torchaudio
 import torch
+from box2kit.utils.load_data import load_mono
 
 codec = dac.DAC.load(dac.utils.download()).to("cuda")
 
-# zeros = torch.zeros(1,1024,2000).to("cuda")
-noise = 10*torch.randn(1,1024,1000).to("cuda")
-noise = codec.quantizer(noise)[0]
-test_audio = codec.decode(noise)
+# # zeros = torch.zeros(1,1024,2000).to("cuda")
+# noise = 10*torch.randn(1,1024,1000).to("cuda")
+# noise = codec.quantizer(noise)[0]
+# test_audio = codec.decode(noise)
 
-torchaudio.save("outs/noise_5e-2.wav", test_audio[0].cpu().detach(), codec.sample_rate)
+latents = codec.encode(load_mono("training_data/drum_kit/1.wav", codec.sample_rate))
+test_audio = codec.decode(-latents)
+
+torchaudio.save("outs/anti_drums.wav", test_audio[0].cpu().detach(), codec.sample_rate)
