feee2dd776aba06b87ce7a38e07da921c0ee977f
miladjavadi <miladjavd@gmail.com>
2025-09-23 16:46:30 +0200

    silence test

--- box2kit/affine/silence_test.py
+++ box2kit/affine/silence_test.py
@@ -10,8 +10,15 @@ codec = dac.DAC.load(dac.utils.download()).to("cuda")
 # noise = codec.quantizer(noise)[0]
 # test_audio = codec.decode(noise)
 
-latents = codec.encode(load_mono("training_data/drum_kit/1.wav", codec.sample_rate).unsqueeze(0).to("cuda")[:,:,:int(4*codec.sample_rate)])[0]
-latents = codec.quantizer(latents)[0]
-test_audio = codec.decode(-latents)
+# latents = codec.encode(load_mono("training_data/drum_kit/1.wav", codec.sample_rate).unsqueeze(0).to("cuda")[:,:,:int(4*codec.sample_rate)])[0]
+# latents = codec.quantizer(latents)[0]
+# test_audio = codec.decode(-latents)
 
-torchaudio.save("outs/anti_drums.wav", test_audio[0].cpu().detach(), codec.sample_rate)
+# torchaudio.save("outs/anti_drums.wav", test_audio[0].cpu().detach(), codec.sample_rate)
+
+silence = torch.zeros(1,1,2*codec.sample_rate).to("cuda")
+
+latents = codec.encode(silence)[0]
+print(torch.mean(latents, dim=-1))
+test_audio = codec.decode(latents)
+torchaudio.save("outs/radio_silence.wav", test_audio[0].cpu().detach(), codec.sample_rate)
