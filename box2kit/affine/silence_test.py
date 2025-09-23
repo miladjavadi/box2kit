2490ee7355b68921e2e9d43761196809b6097efe
miladjavadi <miladjavd@gmail.com>
2025-09-23 15:56:49 +0200

    super noise

--- box2kit/affine/silence_test.py
+++ box2kit/affine/silence_test.py
@@ -5,7 +5,7 @@ import torch
 codec = dac.DAC.load(dac.utils.download()).to("cuda")
 
 # zeros = torch.zeros(1,1024,2000).to("cuda")
-noise = 0.5*torch.randn(1,1024,1000).to("cuda")
+noise = 5*torch.randn(1,1024,1000).to("cuda")
 test_audio = codec.decode(noise)
 
 torchaudio.save("outs/noise_5e-2.wav", test_audio[0].cpu().detach(), codec.sample_rate)
