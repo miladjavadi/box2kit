5a34551b392c49e3cb48f932602232fea6637519
miladjavadi <miladjavd@gmail.com>
2025-09-24 16:35:42 +0200

    kebab kitchen

--- box2kit/affine/transfer.py
+++ box2kit/affine/transfer.py
@@ -38,7 +38,7 @@ def main(args):
             transformed_latents = torch.tensor(np.transpose(transformed_vecs)).reshape(1, 1024, -1).to(DEVICE)
             output_wave = codec.decode(transformed_latents).reshape(1,-1)
 
-            torchaudio.save(f"{OUTPUT_DIR}/{file_name}", output_wave.cpu().numpy()[0], model_sr)
+            torchaudio.save(f"{OUTPUT_DIR}/{file_name}", output_wave.detach().cpu(), model_sr)
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser(description="Train GAN-based timbre transfer model using paired query/carget datasets.\n"
