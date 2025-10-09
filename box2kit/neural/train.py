b4916d8fbc9060afb943ed900bd09833ac0e61e5
miladjavadi <miladjavd@gmail.com>
2025-10-09 11:39:33 +0200

    save periodic checkpoints

--- box2kit/neural/train.py
+++ box2kit/neural/train.py
@@ -103,12 +103,12 @@ def main(args, configs):
     # gan = DACGAN(dac_model, device, block_length_in_samples, output_block_length_in_samples, block_length_in_frames, lambda_embedding=lambda_embedding) # initialize new model
     gan = DACGANV2(block_length_in_samples, output_block_length_in_samples, block_length_in_frames, [dummy_stft.shape[1], dummy_stft.shape[2]], NFFT, beta, phi, codec, warmup=warmup, lr=lr)
 
-    callbacks = []
+    callbacks = [ModelCheckpoint(filename="periodic_{epoch:02d}", every_n_epochs=1000, save_top_k=-1, dirpath="checkpoints/periodic")] # save checkpoints every 1000 epochs
 
     if test_file is not None:
         callbacks.append(GenerationCallback(test_file, test_output_folder, test_freq))
     if val_loader is not None:
-        callbacks.extend([ModelCheckpoint(monitor="val_loss", save_top_k=1, mode="min", filename="best-{epoch:02d}-{val_loss:.2f}", save_last=True)])
+        callbacks.extend([ModelCheckpoint(monitor="val_loss", save_top_k=1, mode="min", filename="best-{epoch:02d}-{val_loss:.2f}", save_last=True, dirpath="checkpoints/best")]) # save best performing model
 
 
     # load from previously saved checkpoint, if provided
